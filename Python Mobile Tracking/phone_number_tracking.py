484314e64dbfd927676468eb17aadf42ce5d65e9
Abdulah-Niaz <abdullahniaz423@gmail.com>
2023-01-25 09:48:22 +0500

    Phone number tacker

--- Python Mobile Tracking/phone_number_tracking.py
+++ Python Mobile Tracking/phone_number_tracking.py
@@ -1,1 +1,13 @@
-import
+import phonenumbers
+from phonenumbers import timezone, geocoder, carrier
+
+# Take a number input from the user & it should be in string
+number = input("Enter the Phone Number With Country Code  ")
+phone = phonenumbers.parse(number)
+time = timezone.time_zones_for_number(phone)
+car = carrier.name_for_number(phone, 'en')
+reg = geocoder.description_for_number(phone, "en")
+geo = geocoder.country_name_for_number(phone, "en")
+
+print("\n\n\n", phone, "\n TimeZone ",
+      time,  " \n Name of company ", car, geo, "\n\n")

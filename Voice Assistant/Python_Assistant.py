0a91aeb9822b0dd93def1bbcdcade21b15e8572b
Abdulah-Niaz <abdullahniaz423@gmail.com>
2023-02-08 13:20:39 +0500

    Assistant updated

--- Voice Assistant/Python_Assistant.py
+++ Voice Assistant/Python_Assistant.py
@@ -56,4 +56,5 @@ def text_to_speech(x):
     Engine.runAndWait()
 
 
-text_to_speech("Get your online store up and running with our e-commerce services on Fiverr. Our team of experts will help you set up a user-friendly, customized e-commerce platform to help you sell your products or services online. We specialize in popular platforms such as Shopify, WooCommerce, and Magento, and can provide everything from setup and configuration to theme customization and payment gateway integration. Trust us to deliver a seamless online shopping experience for your customers.")
+text_to_speech("Hello, I am Docter")
+# text_to_speech("Get your online store up and running with our e-commerce services on Fiverr. Our team of experts will help you set up a user-friendly, customized e-commerce platform to help you sell your products or services online. We specialize in popular platforms such as Shopify, WooCommerce, and Magento, and can provide everything from setup and configuration to theme customization and payment gateway integration. Trust us to deliver a seamless online shopping experience for your customers.")

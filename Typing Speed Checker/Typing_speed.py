b179e44e18846daab6c01b19334cff9d373d3586
Abdulah-Niaz <abdullahniaz423@gmail.com>
2023-02-14 10:43:41 +0500

    Random Numbers Choice

---
+++ Typing Speed Checker/Typing_speed.py
@@ -0,0 +1,9 @@
+from time import *
+import random as ran
+
+list_paragraph = ["my name is abdullhah",
+                  "i am pursuing my computer science deggree", "i love to learn programming"]
+
+choice = ran.choice(list_paragraph)
+print("*********************Typing Speed*********************")
+print(choice)

7261e2f5855d1355410e04cc8401ac0d59a2ed0d
Abdulah-Niaz <abdullahniaz423@gmail.com>
2023-02-14 10:47:21 +0500

    Comments Updated

--- Send Emails Using Python/Semd_Email.py
+++ Send Emails Using Python/Semd_Email.py
@@ -9,11 +9,14 @@ obj.login("Your_gmail_account", "Apps_Password_Not_Gmail")
 subject = "Wordpress Developer"
 body = "Get your online store up and running with our e-commerce services on Fiverr. Our team of experts will help you set up a user-friendly, customized e-commerce platform to help you sell your products or services online. We specialize in popular platforms such as Shopify, WooCommerce, and Magento, and can provide everything from setup and configuration to theme customization and payment gateway integration. Trust us to deliver a seamless online shopping experience for your customers."
 
-
+# Message Type
 message = "Aubject: {}\n\n{}".format(subject, body)
 # you can gave multiple emails here
 list_address = ["Recever email"]
 
+# Sending mail (sender email address,  list of emails receviers , message which you want to send)
 obj.sendmail("Your_gmail_account", list_address, message)
+
+# it will show you message, if message has been send.
 print("Send Email")
 obj.quit()

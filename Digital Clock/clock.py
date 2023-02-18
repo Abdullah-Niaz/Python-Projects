2a2bfba743597b77772bfd6d0c30e4645b9628b2
Abdulah-Niaz <abdullahniaz423@gmail.com>
2023-02-18 22:09:56 +0500

    Colors updated

--- Digital Clock/clock.py
+++ Digital Clock/clock.py
@@ -28,87 +28,87 @@ def date_time():
 clock = Tk()
 clock.title(" Digital Clock ")
 clock.geometry("1000x500")
-clock.config(bg="yellow")
+clock.config(bg="#eeaeca")
 
 lab_hr = Label(clock, text="00", font=(
-    'sans serif', 60, 'bold'), bg="green", fg="black")
+    'sans serif', 60, 'bold'), bg="#94bbe9", fg="black")
 
-lab_hr.place(x=40, y=40, height=110, width=100)
+lab_hr.place(x=100, y=40, height=110, width=100)
 lab_hr_txt = Label(clock, text="Hr", font=(
-    'sans serif', 20, 'bold'), bg="green", fg="black")
+    'sans serif', 20, 'bold'), bg="#94bbe9", fg="black")
 
-lab_hr_txt.place(x=40, y=170, height=60, width=100)
+lab_hr_txt.place(x=100, y=170, height=60, width=100)
 
 
 lab_min = Label(clock, text="00", font=(
-    'sans serif', 60, 'bold'), bg="green", fg="black")
+    'sans serif', 60, 'bold'), bg="#94bbe9", fg="black")
 
-lab_min.place(x=220, y=40, height=110, width=100)
+lab_min.place(x=280, y=40, height=110, width=100)
 
 lab_min_txt = Label(clock, text="Min", font=(
-    'sans serif', 20, 'bold'), bg="green", fg="black")
+    'sans serif', 20, 'bold'), bg="#94bbe9", fg="black")
 
-lab_min_txt.place(x=220, y=170, height=60, width=100)
+lab_min_txt.place(x=280, y=170, height=60, width=100)
 
 
 lab_sec = Label(clock, text="00", font=(
-    'sans serif', 60, 'bold'), bg="green", fg="black")
+    'sans serif', 60, 'bold'), bg="#94bbe9", fg="black")
 
-lab_sec.place(x=380, y=40, height=110, width=100)
+lab_sec.place(x=470, y=40, height=110, width=100)
 lab_sec_txt = Label(clock, text="Sec", font=(
-    'sans serif', 20, 'bold'), bg="green", fg="black")
+    'sans serif', 20, 'bold'), bg="#94bbe9", fg="black")
 
-lab_sec_txt.place(x=380, y=170, height=60, width=100)
+lab_sec_txt.place(x=470, y=170, height=60, width=100)
 
 
 lab_zone = Label(clock, text="00", font=(
-    'sans serif', 60, 'bold'), bg="green", fg="black")
+    'sans serif', 60, 'bold'), bg="#94bbe9", fg="black")
 
-lab_zone.place(x=550, y=40, height=110, width=120)
+lab_zone.place(x=650, y=40, height=110, width=120)
 lab_zone_txt = Label(clock, text="Am/Pm", font=(
-    'sans serif', 20, 'bold'), bg="green", fg="black")
+    'sans serif', 20, 'bold'), bg="#94bbe9", fg="black")
 
-lab_zone_txt.place(x=550, y=170, height=60, width=120)
+lab_zone_txt.place(x=650, y=170, height=60, width=120)
 
 
 lab_date = Label(clock, text="00", font=(
-    'sans serif', 60, 'bold'), bg="green", fg="black")
+    'sans serif', 60, 'bold'), bg="#94bbe9", fg="black")
 
-lab_date.place(x=40, y=300, height=110, width=100)
+lab_date.place(x=100, y=300, height=110, width=100)
 lab_date_txt = Label(clock, text="Date", font=(
-    'sans serif', 20, 'bold'), bg="green", fg="black")
+    'sans serif', 20, 'bold'), bg="#94bbe9", fg="black")
 
-lab_date_txt.place(x=40, y=420, height=60, width=100)
+lab_date_txt.place(x=100, y=420, height=60, width=100)
 
 
 lab_month = Label(clock, text="00", font=(
-    'sans serif', 60, 'bold'), bg="green", fg="black")
+    'sans serif', 60, 'bold'), bg="#94bbe9", fg="black")
 
-lab_month.place(x=220, y=300, height=110, width=100)
+lab_month.place(x=280, y=300, height=110, width=100)
 lab_min_txt = Label(clock, text="Month", font=(
-    'sans serif', 20, 'bold'), bg="green", fg="black")
+    'sans serif', 20, 'bold'), bg="#94bbe9", fg="black")
 
-lab_min_txt.place(x=220, y=420, height=60, width=100)
+lab_min_txt.place(x=280, y=420, height=60, width=100)
 
 
 lab_year = Label(clock, text="00", font=(
-    'sans serif', 60, 'bold'), bg="green", fg="black")
+    'sans serif', 60, 'bold'), bg="#94bbe9", fg="black")
 
-lab_year.place(x=380, y=300, height=110, width=100)
+lab_year.place(x=470, y=300, height=110, width=100)
 lab_year_txt = Label(clock, text="Year", font=(
-    'sans serif', 20, 'bold'), bg="green", fg="black")
+    'sans serif', 20, 'bold'), bg="#94bbe9", fg="black")
 
-lab_year_txt.place(x=380, y=420, height=60, width=100)
+lab_year_txt.place(x=470, y=420, height=60, width=100)
 
 
 lab_day = Label(clock, text="00", font=(
-    'sans serif', 60, 'bold'), bg="green", fg="black")
+    'sans serif', 60, 'bold'), bg="#94bbe9", fg="black")
 
-lab_day.place(x=550, y=300, height=110, width=120)
+lab_day.place(x=650, y=300, height=110, width=120)
 lab_day_txt = Label(clock, text="Day", font=(
-    'sans serif', 20, 'bold'), bg="green", fg="black")
+    'sans serif', 20, 'bold'), bg="#94bbe9", fg="black")
 
-lab_day_txt.place(x=550, y=420, height=60, width=120)
+lab_day_txt.place(x=650, y=420, height=60, width=120)
 
 
 date_time()
